b99d0b215d2c212360760bee477a4cb8fab23533
Bilal Yaver <bilalyaver@outlook.com>
2025-09-25 11:02:00 +0300

    fix: remove authGuard from health check route

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,9 @@ const router = Router();
 import authRoutes from "@modules/auth/auth.routes";
 import tenantRoutes from "@modules/tenants/tenant.routes";
 import usersRoutes from "@modules/users/user.routes";
-import { authGuard } from "@core/middlewares/auth.guard";
 
 // Health check
-router.get("/health",authGuard, (_req, res) => {
+router.get("/health", (_req, res) => {
     res.sendSuccess({ ok: true }, "Server is healthy");
 });
 
